667f99c1f0c76bb6bd358d4ebe0c3483573d6d8c
Robin <daorob@gmail.com>
2020-03-29 16:22:04 -0500

    Implement a classback-based solution to figure out if an item is a file or a folder.

--- index.js
+++ index.js
@@ -12,14 +12,29 @@ fs.readdir(process.cwd(), (error, filenames) => {
         // return;
     }
 
+    // this creates an array with values of null matching the length of filenames
+    const allStats = Array(filenames.length).fill(null);
+
     // how to solve this the incorrect way
     for (let filename of filenames) {
+        const index = filenames.indexOf(filename);
+
         fs.lstat(filename, (err, stats) => {
             if (err) {
                 console.log(err);
             }
 
-            console.log(filename, stats.isFile());
+            allStats[index] = stats;
+
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(filenames[index], stats.isFile());
+                })
+            }
         });
     }
 });
