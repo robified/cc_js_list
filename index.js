ad12d993463a7c64ca312812de502b4c760a2b4e
Robin <daorob@gmail.com>
2020-03-29 15:14:16 -0500

    Successfully list files in current directory.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const fs = require('fs');
+
+fs.readdir('.', (error, filenames) => {
+    // handle either the error or carry on
+    if (error) {
+        // the error handling code here
+        console.log(error);
+        // can either throw a new error or just stop
+        // throw new Error(error);
+        // return;
+    }
+
+    console.log(filenames);
+});
